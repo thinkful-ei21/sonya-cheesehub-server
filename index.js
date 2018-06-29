30b0ef48e6283906a60236942274a62f5f58af5b
Sonya Slegers <sonjay08@gmail.com>
2018-06-29 11:30:11 -0500

    added /cheeses api call

--- index.js
+++ index.js
@@ -22,6 +22,37 @@ app.use(
   })
 );
 
+const cheeses = [
+  'Bath Blue',
+  'Barkham Blue',
+  'Buxton Blue',
+  'Cheshire Blue',
+  'Devon Blue',
+  'Dorset Blue Vinney',
+  'Dovedale',
+  'Exmoor Blue',
+  'Harbourne Blue',
+  'Lanark Blue',
+  'Lymeswold',
+  'Oxford Blue',
+  'Shropshire Blue',
+  'Stichelton',
+  'Stilton',
+  'Blue Wensleydale',
+  'Yorkshire Blue'
+];
+
+app.get('/api/cheeses', (req, res) => {
+
+  res.json(cheeses)
+    .catch(err => {
+      console.error({
+        status: err.status,
+        mesage: err.message
+      });
+    });
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
